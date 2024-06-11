f029fe2042831644466bd3d4bd85ed9db1772467
Abdullah Khan <172060277+abdullahkhan-ceo@users.noreply.github.com>
2024-06-11 12:34:58 +0530

    Update vite.config.js
    Added the outputDir

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react()],
   server: {
     port: 3000
+  },
+  build: {
+    outDir: 'dist' // Ensure this matches the output directory in your GitHub Actions workflow
   }
-  
 })
-// vite.config.js
-
-
-
-
