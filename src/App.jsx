43bd04136da4083cc86f809e85e81325e472b0bb
Abdullah Khan <172060277+abdullahkhan-ceo@users.noreply.github.com>
2024-06-11 14:52:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Signup from './screens/Auth/Signup'
-import Home from './screens/Home/Home'
-import Layout from './components/Layout'
-import Analytics from './screens/Analytics/index'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './screens/Auth/Signup';
+import Home from './screens/Home/Home';
+import Layout from './components/Layout';
+import Analytics from './screens/Analytics/index';
 import './index.css';
-import AdIdea from './screens/adIdea'
-
-
+import AdIdea from './screens/adIdea';
 
 function App() {
-
-
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Signup />} />
-      <Route element={<Layout />}>
-      <Route path="/home" element={<Home />} />
-      <Route path="/analytics" element={<Analytics />} />
-      <Route path="/adidea" element={<AdIdea />} />
-      </Route>
-  
-    </Routes>
-  </Router>
-  )
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route element={<Layout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/adidea" element={<AdIdea />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
